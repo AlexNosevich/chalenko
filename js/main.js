49041915e23edf43390f67b9ab859360050cd781
AlexNosevich <chivesonprojects@gmail.com>
2021-07-18 17:12:08 +0300

    Завершен десктоп. Тест php

--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
-new Splide( '.splide' ).mount();
+var splide = new Splide( '.splide' );
+
+splide.on( 'autoplay:playing', function ( rate ) {
+	console.log( rate ); 
+} );
+
+splide.mount();
+
+ var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+
+  function openModal () {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+    $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+  }
+  function closeModal (event) {
+    event.preventDefault ();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible'); 
+    
+  }
+
+ // Обработка формы
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: "Имя должно быть длиннее 2-х символов",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Пожалуйста, введите свой номер телефона",
+          phone: "Ваш номер должен быть в формате 89999999999",
+          minlength: "Ваш номер должен быть длиннее 10-ти символов",
+
+        },
+      },
+    });
+  })
+  $('.phone').mask('+7 (000)-000-00-00');
+  $( ".phone" ).validate({
+    rules: {
+      field: {
+        required:"More that 10 symbols",
+        minlength: "More that 10 symbols",
+        
+      }
+    }
+  });
